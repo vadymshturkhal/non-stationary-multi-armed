ad2dbda7c261fa6598a0e11b8697280a60b3bb9c
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-03-30 12:28:36 +0200

    Subtract start points from epoch points every  epoch, deleted stationary code.

--- plot_data.py
+++ plot_data.py
@@ -1,30 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from settings import stationary_bandit_data_bar_filename, stationary_bandit_data_average_reward
 from utils import DB_Operations
 
-
-def plot_stationary_bar_data():
-    # Load the results from a CSV file
-    df = pd.read_csv(stationary_bandit_data_bar_filename)
-
-    # Create the plot
-    plt.figure(figsize=(10, 6))
-    bars = plt.bar(df['Action'], df['Number of Times Chosen'], color='skyblue')
-
-    # Annotate bars with the estimated and true probabilities
-    for bar, est_prob, true_prob in zip(bars, df['Estimated Action Values'], df['True Action Values']):
-        yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width()/2, yval + 1, f'Est: {est_prob:.2f}\nTrue: {true_prob:.2f}', 
-                    ha='center', va='bottom', color='black', fontsize=8)
-
-    # Set the labels and title
-    plt.xlabel('Arm Number')
-    plt.ylabel('Times Chosen')
-    plt.title('Number of Times Each Arm Has Been Chosen')
-    plt.xticks(df['Action'])
-    plt.show()
 
 def plot_average_rewards(file_path: str):
     # Load the data from the CSV file
--- start.py
+++ start.py
@@ -1,6 +1,5 @@
 from agent import NonStationaryAgent, NonStationaryAgentBet, NonStationaryAgentUCB
 from game_environment import MultiArmedGame
-from plot_data import plot_rewards
 from settings import START_POINT, nonstationary_bandit_data_average_reward, BET
 from utils import DB_Operations, create_average_data
 
@@ -36,8 +35,7 @@ def start_epoch(main_agent, main_agent_params, support_agent, support_agent_para
         support_agent_instance = support_agent(*support_agent_params)
 
         epoch_reward = train(game, agent_instance, support_agent_instance, steps)
-        if epoch_reward < START_POINT:
-            epoch_reward = epoch_reward - START_POINT
+        epoch_reward = epoch_reward - START_POINT
 
         cost += epoch_reward
         create_average_data(nonstationary_bandit_data_average_reward, support_agent_instance, rewards, betting)
@@ -50,8 +48,8 @@ if __name__ =='__main__':
     k = 1  # Number of actions (bandits)
     epsilon = 0.2  # Exploration probability
     alpha = 0.5
-    steps = 1000
-    epochs = 40
+    steps = 10000
+    epochs = 10
     rewards = []
     betting = []
 
@@ -68,4 +66,3 @@ if __name__ =='__main__':
     # support_agent_params = [k, alpha]
 
     print(start_epoch(main_agent, main_agent_params, support_agent, support_agent_params))
-    # plot_rewards(nonstationary_bandit_data_average_reward)
