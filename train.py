5870b58342b0e804c632d45391dbdac43a9b7847
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-01 11:22:32 +0300

    Created class for training.

--- train.py
+++ train.py
@@ -1,73 +1,69 @@
-from agent import NonStationaryAgent, NonStationaryAgentBet, NonStationaryAgentUCB, TDZero
+from agent import NonStationaryAgent, NonStationaryAgentBet, TDZero
 from game_environment import MultiArmedGame
-from settings import START_POINT, nonstationary_bandit_data_average_reward, BET
-from utils import DB_Operations, create_average_data
+from settings import START_POINT, BET
+from utils import DB_Operations
 
 
-def train(game: MultiArmedGame, main_agent, support_agent: TDZero, steps=1000):
-    rewards.clear()
-    betting.clear()
-    for _ in range(steps):
-        state = support_agent.get_state()
-    
-        choose_dealer = main_agent.choose_action()
-        action_bet = support_agent.choose_action()
+class TrainAgent:
+    def __init__(self, game, main_agent, bet_agent):
+        self.game = game
+        self.main_agent = main_agent
+        self.bet_agent = bet_agent
 
-        reward = game.apply_action(choose_dealer, bet=action_bet)
+    def train(self, games=1000):
+        rewards.clear()
+        betting.clear()
+        for _ in range(games):
+            state = self.bet_agent.get_state()
+        
+            choose_dealer = self.main_agent.choose_action()
+            action_bet = self.bet_agent.choose_action()
 
-        game.play_step()
+            reward = self.game.apply_action(choose_dealer, bet=action_bet)
 
-        last_bet = game.last_bet
-        is_end = support_agent.update_points(last_bet, reward)
+            self.game.play_step()
 
-        next_state = support_agent.get_state()
+            last_bet = self.game.last_bet
+            is_game_end = self.bet_agent.update_points(last_bet, reward)
 
-        main_agent.update_estimates(choose_dealer, reward - last_bet)
-        support_agent.update_estimates(state, action_bet, reward - last_bet, next_state)
+            state_next = self.bet_agent.get_state()
 
-        rewards.append(reward)
-        betting.append(last_bet)
+            self.main_agent.update_estimates(choose_dealer, reward - last_bet)
+            self.bet_agent.update_estimates(state, action_bet, reward - last_bet, state_next)
 
-        if is_end:
-            break
-    return support_agent.points
+            rewards.append(reward)
+            betting.append(last_bet)
 
-def start_epoch(main_agent, main_agent_params, support_agent, support_agent_params):
-    cost = 0
-    db_operations = DB_Operations()
-    for epoch in range(epochs):
-        game = MultiArmedGame(k, speed=60, is_rendering=False)
-        agent_instance = main_agent(*main_agent_params)
-        support_agent_instance = support_agent(*support_agent_params)
+            if is_game_end:
+                break
 
-        epoch_reward = train(game, agent_instance, support_agent_instance, steps)
-        epoch_reward = epoch_reward - START_POINT
+        return self.bet_agent.points
 
-        cost += epoch_reward
-        db_operations.add_epoch_to_db(epoch, epoch_reward, support_agent_instance, rewards, betting)
+    def train_epoch(self, epochs_quantity=1, games_in_epoch=1000):
+        cost = 0
+        db_operations = DB_Operations()
+        for epoch in range(epochs_quantity):
+            epoch_reward = self.train(games=games_in_epoch)
+            epoch_reward = epoch_reward - START_POINT
 
-    return cost
+            cost += epoch_reward
+            db_operations.add_epoch_to_db(epoch, epoch_reward, self.support_agent, rewards, betting)
+
+        return cost
 
 
 if __name__ =='__main__':
-    k = 1  # Number of actions (bandits)
+    k = 1  # Number of bandits
     epsilon = 0.1  # Exploration probability
-    alpha = 0.01
-    steps = 1000
+    alpha = 0.1
+    games = 1000
     epochs = 10
     rewards = []
     betting = []
 
-    main_agent = NonStationaryAgent
-    main_agent_params = [k, epsilon, alpha]
+    game = MultiArmedGame(k, speed=60, is_rendering=False) 
+    main_agent = NonStationaryAgent(k, epsilon, alpha)
+    bet_agent = TDZero(len(BET), epsilon, alpha)
 
-    support_agent = TDZero
-    support_agent_params = [len(BET), epsilon, alpha]
-
-    # main_agent = NonStationaryAgentUCB
-    # main_agent_params = [k, alpha]
-
-    # support_agent = NonStationaryAgentUCB
-    # support_agent_params = [k, alpha]
-
-    print(start_epoch(main_agent, main_agent_params, support_agent, support_agent_params))
+    ta = TrainAgent(game=game, main_agent=main_agent, bet_agent=bet_agent)
+    print(ta.train(games=games))
