d9f4a7461759144785b618930e036c082298048a
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-02 17:51:32 +0300

    Added property times win, added method get_state to game stats class.

--- game_stats.py
+++ game_stats.py
@@ -1,18 +1,22 @@
 import numpy as np
 
-from settings import BET
+from settings import BET, END_MULTIPLIER, MIN_POINTS_MULTIPLIER, START_POINT
 
 
 class GameStats:
     def __init__(self) -> None:
-        self._hands_played = 0
+        self.hands_played = 0
         self._total_score = 0
         self._win_bet = np.zeros(len(BET))
         self._times_choose_bet = np.full(len(BET), np.finfo(np.float32).tiny)
         self._prob_bet = np.zeros(len(BET))
 
+    @property
+    def times_win(self):
+        return np.sum(self._win_bet)
+
     def add_hand_reward(self, bet, reward):
-        self._hands_played += 1
+        self.hands_played += 1
         self._total_score += reward
         self._times_choose_bet[bet]+= 1
 
@@ -22,8 +26,17 @@ class GameStats:
         self._prob_bet = self._win_bet / self._times_choose_bet
 
     def reset(self):
-        self._hands_played = 0
-        self._win_times = 0
+        self.hands_played = 0
+        self._total_score = 0
         self._win_bet.clear()
         self._times_choose_bet.clear()
         self._prob_bet.clear()
+
+    def get_state(self):
+        general_win_probability = np.sum(self._win_bet) / self.hands_played
+        state = np.array([
+            *self._win_bet,
+            *self._times_choose_bet,
+            *self._prob_bet,
+        ])
+        return state
