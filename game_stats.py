9caf34e429ad7d317a46a3cd902cb39a680256f6
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-04-02 13:43:47 +0300

    Updated game stats class.

--- game_stats.py
+++ game_stats.py
@@ -1,17 +1,25 @@
+import numpy as np
+
 from settings import BET
 
 
 class GameStats:
-    def __init__(self, game) -> None:
-        self.game = game
+    def __init__(self) -> None:
         self._hands_played = 0
-        self._win_bet = [0] * len(BET)
-        self._times_choose_bet = [0] * len(BET)
-        self._prob_bet = [0] * len(BET)
+        self._total_score = 0
+        self._win_bet = np.zeros(len(BET))
+        self._times_choose_bet = np.full(len(BET), np.finfo(np.float32).tiny)
+        self._prob_bet = np.zeros(len(BET))
 
-    def add_hand_reward(self, action, reward):
-        self._win_times += 1 if reward > 0 else 0
+    def add_hand_reward(self, bet, reward):
+        self._hands_played += 1
+        self._total_score += reward
+        self._times_choose_bet[bet]+= 1
 
+        if reward > 0:
+            self._win_bet[bet] += 1
+
+        self._prob_bet = self._win_bet / self._times_choose_bet
 
     def reset(self):
         self._hands_played = 0
